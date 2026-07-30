d399d0d565e935b87f1284683b317d0fb362d2c6
mateusbadalotti <mateus.badalotti@gmail.com>
2026-07-30 14:23:20 -0300

    Use versioned release URL

--- Casks/audio-priority.rb
+++ Casks/audio-priority.rb
@@ -2,7 +2,7 @@ cask "audio-priority" do
   version "2.1.0"
   sha256 "58094012fdd12768f75f06d0521cbdf21c43d93779674d9daf61aabfeb4870d1"
 
-  url "https://github.com/mateusbadalotti/audio-priority/releases/download/v2.1.0/AudioPriority.zip"
+  url "https://github.com/mateusbadalotti/audio-priority/releases/download/v#{version}/AudioPriority.zip"
   name "Audio Priority"
   desc "Menu bar app that manages audio device priority"
   homepage "https://badalotti.dev/audio-priority"
